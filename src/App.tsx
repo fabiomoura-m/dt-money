487382d9f4555d3d11aca416cd020cc16c7411bf
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-09-27 14:17:05 -0300

    Estrutura visual - Setup do projeto
    Adicionado cores default e estilos globais do projeto utilizando o styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
+
 export function App() {
     return (
-        <div>
-            <h1>Teste</h1>
-        </div>
+        <ThemeProvider theme={defaultTheme}>
+            <GlobalStyle />
+            <div>
+                <h1>Hello World</h1>
+            </div>
+        </ThemeProvider>
     );
 }
