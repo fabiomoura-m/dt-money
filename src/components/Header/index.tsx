e151121f77eb5733e2638e78b0157f39dabc367f
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-09-27 18:07:01 -0300

    Estrutura visual - Componente: Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './style';
+import logoImage from '../../assets/logo.svg';
+
+export function Header() {
+    return (
+        <HeaderContainer>
+            <HeaderContent>
+                <img src={logoImage} alt="" />
+                <NewTransactionButton>Nova Transação</NewTransactionButton>
+            </HeaderContent>
+        </HeaderContainer>
+    );
+}
