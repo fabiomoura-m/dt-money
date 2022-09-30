870bd9771971c7cbb5e06a09cbae2ece1432ed7e
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-09-30 10:54:17 -0300

    Conexão com a API - Calculando resumo de transações

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -8,6 +8,25 @@ export function Summary() {
     const theme = useTheme();
     const { transactions } = useContext(TransactionsContext);
 
+    const summary = transactions.reduce(
+        (acc, transaction) => {
+            if (transaction.type === 'income') {
+                acc.income += transaction.price;
+                acc.total += transaction.price;
+            } else {
+                acc.outcome += transaction.price;
+                acc.total -= transaction.price;
+            }
+
+            return acc;
+        },
+        {
+            income: 0,
+            outcome: 0,
+            total: 0
+        }
+    );
+
     return (
         <SummaryContainer>
             <SummaryCard>
@@ -15,21 +34,21 @@ export function Summary() {
                     <span>Entradas</span>
                     <ArrowCircleUp size={32} color={theme['green-300']} />
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>{summary.income}</strong>
             </SummaryCard>
             <SummaryCard>
                 <header>
                     <span>Saídas</span>
                     <ArrowCircleDown size={32} color={theme['red-300']} />
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>{summary.outcome}</strong>
             </SummaryCard>
             <SummaryCard variant="green">
                 <header>
                     <span>Total</span>
                     <CurrencyDollar size={32} color={theme.white} />
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>{summary.total}</strong>
             </SummaryCard>
         </SummaryContainer>
     );
